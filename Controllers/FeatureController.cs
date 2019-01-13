dff469bfcd656300c6098c0d92b6bd9a8b1b23b7
Peter Acs <peter.acs@panoramic.systems>
2019-01-13 19:18:53 +0000

    Add FeatureController and Resource

---
+++ Controllers/FeatureController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using cardealer.Models;
+using cardealer.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace cardealer.Controllers
+{
+	public class FeatureController : Controller
+	{
+		private readonly CardealerDbContext _context;
+		private readonly IMapper _mapper;
+
+		public FeatureController(CardealerDbContext context, IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+		}
+
+		[HttpGet("/api/features")]
+		public async Task<IEnumerable<FeatureResource>> GetFeaturesAsync()
+		{
+			var features = await _context.Features.ToListAsync().ConfigureAwait(false);
+
+			return _mapper.Map<List<Feature>, List<FeatureResource>>(features);
+		}
+	}
+}
