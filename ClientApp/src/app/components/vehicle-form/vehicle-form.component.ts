ce70f284cdeda38cd8aac0967d5459670a90a57c
Peter Acs <peter.acs@panoramic.systems>
2019-01-13 21:27:12 +0000

    Add 'new vehicle form'

---
+++ ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MakeService } from '../../services/make.service';
+
+@Component({
+  selector: 'app-vehicle-form',
+  templateUrl: './vehicle-form.component.html',
+  styleUrls: ['./vehicle-form.component.css']
+})
+export class VehicleFormComponent implements OnInit {
+  makes : any[];
+  vehicle : any = {};
+  models: any[];
+
+  constructor(private makeService: MakeService) { }
+
+  ngOnInit() {
+    this.makeService.getMakes().subscribe(makes => {
+      this.makes = makes;
+    });
+  }
+
+  onMakeChange() {
+    let selectedMake = this.makes.find(make => make.id == this.vehicle.make);
+    this.models = selectedMake ? selectedMake.models : [];
+  }
+}
